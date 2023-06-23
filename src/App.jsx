905503a8aee083dbedbb6f761d9476e68cce974a
ioangheraszim <gijohny17@gmail.com>
2023-06-24 00:33:57 +0100

    added data from a separate file called data.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Components from "./components/Components"
+import data from "./data"
 function App() {
 
+  const myData = data.map(item => {
+    return <Components 
+      key = {item.id}
+      {...item}
+    />
+  })
   return (
     <main className="container">
      <Navbar />
-     <Components />
+     {myData}
     </main>
   )
 }
--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -4,17 +4,17 @@ export default function Components(props) {
     return (
         <section className="card">
             <div className="left-side">
-                <img className="card-img" src="https://images.unsplash.com/photo-1570459027562-4a916cc6113f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1888&q=80" alt="" />   
+                <img className="card-img" src={props.imageUrl} alt="" />   
             </div>
             <div className="right-side">
                 <div className="right-info">
                     <img className="placeholder-img" src="./src/assets/placeholder.png" alt="placeholder image" />
-                    <p className="location-name">Japan</p>
-                    <a className="location-link" href="#">View on Google Maps</a>
+                    <p className="location-name">{props.location}</p>
+                    <a className="location-link" href={props.googleMapsUrl}>View on Google Maps</a>
                 </div>
-                <h2 className="card-title">Mount Fuji</h2>
-                <p className="card-date">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="card-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-date">{props.startDate} - {props.endDate}</p>
+                <p className="card-description">{props.description}</p>
             </div>
         </section>
         
